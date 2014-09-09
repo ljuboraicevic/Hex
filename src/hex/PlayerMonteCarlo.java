2a3d978d4552dec7b3cfcf590034a5219e569ecd
Nikola Radovic <nikolaradovic85@gmail.com>
2014-09-09 14:25:20 +0200

    added didIWin method in PlayerMonteCarlo added addition private help methods needed for didIWin

--- src/hex/PlayerMonteCarlo.java
+++ src/hex/PlayerMonteCarlo.java
@@ -10,4 +10,110 @@ public class PlayerMonteCarlo implements Player {
     public Coordinate makeMove(Table t) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    
+    /**
+     * Checks if player won game. Checks from top to bottom in each row if there is vertical player
+     * fields (1) that are somehow connected to active vertical players fields from previous row
+     * If it doesn't find any active vertical field in row, then halt => player two won
+     * 
+     * @param t
+     * @param player == 0 => vertical player's move, player == 1 => horizontal player's move
+     * @return 
+     */
+    private boolean didIWin(Table t, byte player) {
+        //booleans that indicate that on [i] position in currentRow there is active vertical field
+        //field that's marked by player0
+        boolean[] activeVerticalFieldsInRow = new boolean[t.size];
+        //for first row all "previous" row fields are active 
+        for (int i = 0; i < t.size; i++) {
+            activeVerticalFieldsInRow[i] = true;
+        }
+        int currentRow = 0;
+        boolean winnerUndetermined = true;
+        byte playerWon = 0;
+        
+        while (winnerUndetermined && currentRow < t.size) {
+            activeVerticalFieldsInRow = getPotentialsInRow(t, currentRow, activeVerticalFieldsInRow);
+            checkForMissedActiveFields(t, currentRow, activeVerticalFieldsInRow);
+            if (anyVerticalPlayerOnPotentialFieldInRow(t, currentRow, activeVerticalFieldsInRow)) {
+                currentRow++;
+            } else {
+                winnerUndetermined = false;
+                playerWon = 1;
+            }
+        }
+        return playerWon == player;
+    }
+    /**
+     * checks if there is any active field in currentRow
+     * @param t
+     * @param row
+     * @param activeFields
+     * @return 
+     */
+    private boolean anyVerticalPlayerOnPotentialFieldInRow(Table t, int row, boolean[] activeFields) {
+        for (int i = 0; i < t.size; i++) {
+            if (activeFields[i] && t.isFieldVertical(new Coordinate(row, i))) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+    /**
+     * marks all fields that are active based on connection with active fields from previous row
+     * this doesn't find all active fields in row, only ones that are connected with previous row
+     * ex: t = 
+     * 1 2 2
+     * 1 1 1
+     * 2 2 1
+     * t[1][1] won't be marked as active, because fields from previous row that t[1][1]is connected to
+     * (t[0][1],t[0][2]) are not active
+     * @param t
+     * @param row
+     * @param previousRowActiveFields
+     * @return 
+     */
+    private boolean[] getPotentialsInRow(Table t, int row, boolean[] previousRowActiveFields) {
+        boolean potentials[] = new boolean[t.size];
+        for (int i = 0; i < t.size - 1; i++) {
+            potentials[i] = t.isFieldVertical(new Coordinate(row, i)) && (previousRowActiveFields[i] || previousRowActiveFields[i + 1]);
+        }
+        potentials[t.size - 1] = t.isFieldVertical(new Coordinate(row, t.size - 1))
+                && previousRowActiveFields[t.size - 1];
+        return potentials;
+    }
+    /**
+     * finds active fields in row that are horizontally connected to some active field
+     * It has to be started after getPotentialsInRow()
+     * This function marks t[1][1] and t[1][2] from previous example as active
+     * based on connection with t[1][0] that is marked as active by getPotentialsInRow()
+     * @param t
+     * @param currentRow
+     * @param activeVerticalFieldsInRow 
+     */
+    private void checkForMissedActiveFields(Table t, int currentRow, boolean[] activeVerticalFieldsInRow) {
+        for (int i = 1; i < t.size; i++) {
+            if (!activeVerticalFieldsInRow[i] && t.isFieldVertical(new Coordinate(currentRow, i))) {
+                activeVerticalFieldsInRow[i] = activeVerticalFieldsInRow[i - 1];
+            }
+        }
+        for (int i = t.size - 2; i > 0; i--) {
+            if (!activeVerticalFieldsInRow[i] && t.isFieldVertical(new Coordinate(currentRow, i))) {
+                activeVerticalFieldsInRow[i] = activeVerticalFieldsInRow[i + 1];
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        for (int i = 1; i < 10; i++) {
+//            String filename = "input"+i+".txt";
+//            Table t = new Table(filename);
+//            PlayerMonteCarlo pmc = new PlayerMonteCarlo();
+//            System.out.println(t);
+//            System.out.println("Player1 won: "+pmc.didIWin(t, (byte) 0));
+//            System.out.println("Player2 won: "+pmc.didIWin(t, (byte) 1));
+//        }
+//    }
 }
--- src/hex/Table.java
+++ src/hex/Table.java
@@ -1,23 +1,29 @@
 package hex;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Class <tt>Table</tt> represents the table on which the Hex game is played.
- * 
+ *
  * @author Ljubo Raicevic <rljubo90@gmail.com>
  */
 public class Table {
+
     /**
      * Matrix of bytes representing past players' past moves.
      */
     byte[][] matrix;
-    
+
     /**
      * Number of empty fields left on the table.
      */
     int noOfEmptyFields;
-    
+
     /**
      * Table side size.
      */
@@ -25,18 +31,39 @@ public class Table {
 
     /**
      * Initializes an empty Table.
-     * 
+     *
      * @param size Table size
      */
     public Table(int size) {
         this.matrix = new byte[size][size];
         this.size = size;
-        this.noOfEmptyFields = size*size;
+        this.noOfEmptyFields = size * size;
     }
-    
+
+    /**
+     * 
+     * Creates table from file
+     * @param filename 
+     */
+    public Table(String filename) {
+        try {
+            Scanner scan = new Scanner(new File(filename));
+            this.size = scan.nextInt();
+            this.matrix = new byte[size][size];
+            this.noOfEmptyFields = 0;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    matrix[i][j] = scan.nextByte();
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PlayerMonteCarlo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     /**
      * Puts a "mark" on a field
-     * 
+     *
      * @param c Coordinates of the field
      * @param mark 1 or 2
      * @return true if move is legal, false otherwise
@@ -48,20 +75,20 @@ public class Table {
             noOfEmptyFields--;
             return true;
         }
-        
+
         //if the field has already been marked return false
         return false;
     }
 
     /**
-     * Returns an ArrayList with coordinates of empty fields on the table. 
+     * Returns an ArrayList with coordinates of empty fields on the table.
      * O(size^2)
-     * 
+     *
      * @return coordinates of empty fields
      */
     public ArrayList<Coordinate> getEmptyFields() {
         ArrayList<Coordinate> result = new ArrayList<>();
-        
+
         //for each field on the table
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
@@ -72,13 +99,13 @@ public class Table {
                 }
             }
         }
-        
+
         return result;
     }
-    
+
     /**
      * Checks if a field has already been marked.
-     * 
+     *
      * @param c Coordinates of the field.
      * @return true if field has been marked, false otherwise
      */
@@ -89,23 +116,26 @@ public class Table {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        
+
         for (int row = 0; row < size; row++) {
-            
+
             //add empty spaces to make romboid shape
             for (int iCount = 0; iCount < row; iCount++) {
                 sb.append(" ");
             }
-            
+
             for (int col = 0; col < size; col++) {
                 sb.append(matrix[row][col]).append(" ");
             }
-            
+
             sb.append(System.lineSeparator());
         }
-        
+
         return sb.toString();
     }
-    
-    
+
+    public boolean isFieldVertical(Coordinate c) {
+        return matrix[c.row][c.col] == 1;
+    }
+
 }
